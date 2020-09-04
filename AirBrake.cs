f7e2dfa6499d5ac5dfe6f82d14092155345c58ef
mspielberg <9729801+mspielberg@users.noreply.github.com>
2020-09-04 10:19:30 -0700

    add mod scaffold

--- AirBrake.cs
+++ AirBrake.cs
@@ -1,8 +1,75 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
+using UnityModManagerNet;
 
 namespace DvMod.AirBrake
 {
-    public static class AirBrake
+    public static class Main
     {
+        public static UnityModManager.ModEntry mod;
+        public static Settings settings;
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            mod = modEntry;
+
+            try { settings = Settings.Load<Settings>(modEntry); } catch {}
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+
+            modEntry.OnGUI = OnGui;
+            modEntry.OnSaveGUI = OnSaveGui;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnUnload = OnUnload;
+
+            return true;
+        }
+
+        static void OnGui(UnityModManager.ModEntry modEntry)
+        {
+            settings.Draw(modEntry);
+        }
+
+        static void OnSaveGui(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            if (value != enabled)
+            {
+                enabled = value;
+            }
+            return true;
+        }
+
+        static bool OnUnload(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.UnpatchAll(modEntry.Info.Id);
+            return true;
+        }
+
+        public static void DebugLog(string message)
+        {
+            if (settings.enableLogging)
+                mod.Logger.Log(message);
+        }
+
+        public class Settings : UnityModManager.ModSettings, IDrawable
+        {
+
+            [Draw("Enable logging")] public bool enableLogging = false;
+
+            override public void Save(UnityModManager.ModEntry entry) {
+                Save<Settings>(this, entry);
+            }
+
+            public void OnChange() {
+            }
+        }
+
     }
 }
