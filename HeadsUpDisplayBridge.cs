118031104e4056f19486dca3aca173f0171620b6
mspielberg <9729801+mspielberg@users.noreply.github.com>
2020-09-21 09:30:40 -0700

    Change formatting of brake pressure information

--- HeadsUpDisplayBridge.cs
+++ HeadsUpDisplayBridge.cs
@@ -26,35 +26,35 @@ namespace DvMod.AirBrake
         private void Register()
         {
             PushProvider auxReservoirPressureProvider = new PushProvider(
-                "Aux reservoir pressure", () => true, v => v.ToString("F2"));
+                "Aux reservoir", () => true, v => $"{v:F2} bar");
             foreach (var nonLocoType in Enum.GetValues(typeof(TrainCarType)).OfType<TrainCarType>().Where(t => !CarTypes.IsLocomotive(t)))
                 Registry.Register(nonLocoType, auxReservoirPressureProvider);
 
             PushProvider brakeCylinderPressureProvider = new PushProvider(
-                "Brake cylinder pressure", () => true, v => v.ToString("F2"));
+                "Brake cylinder", () => true, v => $"{v:F2} bar");
             Registry.Register(RegistryKeys.AllCars, brakeCylinderPressureProvider);
 
             PushProvider equalizationReservoirPressureProvider = new PushProvider(
-                "Equalizing reservoir pressure", () => true, v => v.ToString("F2"));
+                "Equalizing reservoir", () => true, v => $"{v:F2} bar");
             foreach (var locoType in CarTypes.locomotivesMap)
                 Registry.Register(locoType, equalizationReservoirPressureProvider);
         }
 
         public void UpdateAuxReservoirPressure(TrainCar car, float pressure)
         {
-            if (Registry.GetProvider(TrainCarType.NotSet, "Aux reservoir pressure") is PushProvider pp)
+            if (Registry.GetProvider(TrainCarType.NotSet, "Aux reservoir") is PushProvider pp)
                 pp.MixSmoothedValue(car, pressure);
         }
 
         public void UpdateBrakeCylinderPressure(TrainCar car, float brakeCylinderPressure)
         {
-            if (Registry.GetProvider(TrainCarType.NotSet, "Brake cylinder pressure") is PushProvider pp)
+            if (Registry.GetProvider(TrainCarType.NotSet, "Brake cylinder") is PushProvider pp)
                 pp.MixSmoothedValue(car, brakeCylinderPressure);
         }
 
         public void UpdateEqualizingReservoirPressure(TrainCar car, float equalizingReservoirPressure)
         {
-            if (Registry.GetProvider(car.carType, "Equalizing reservoir pressure") is PushProvider pp)
+            if (Registry.GetProvider(car.carType, "Equalizing reservoir") is PushProvider pp)
                 pp.MixSmoothedValue(car, equalizingReservoirPressure);
         }
     }
