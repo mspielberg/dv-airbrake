3d2f889fd5d9f8e81b00cdafc132601c45d50a1e
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-03-11 18:37:59 -0800

    Persist main reservoir pressure in save

--- SaveManager.cs
+++ SaveManager.cs
@@ -1,12 +1,14 @@
 using HarmonyLib;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace DvMod.AirBrake
 {
     public static class AirBrakeSaveManager
     {
         private const string SaveKey = "DvMod.AirBrake";
+        private const string mainReservoirKey = "mainReservoirPressure";
 
         [HarmonyPatch(typeof(CarsSaveManager), nameof(CarsSaveManager.GetCarSaveData))]
         public static class GetCarSaveDataPatch
@@ -14,14 +16,17 @@ namespace DvMod.AirBrake
             public static void Postfix(TrainCar car, JObject __result)
             {
                 var state = ExtraBrakeState.Instance(car.brakeSystem);
-                if (state.Valid)
-                {
-                    Main.DebugLog($"Saving state for {car.ID}: {state}");
-                    __result[SaveKey] = JObject.FromObject(state);
-                }
-                else
+                if (!state.Valid)
                 {
                     Main.DebugLog($"Skipping corrupted ExtraBrakeState {state} for {car.ID}");
+                    return;
+                }
+
+                Main.DebugLog($"Saving state for {car.ID}: {state}");
+                __result[SaveKey] = JObject.FromObject(state);
+                if (CarTypes.IsLocomotive(car.carType))
+                {
+                    __result[SaveKey][mainReservoirKey] = car.brakeSystem.mainReservoirPressureUnsmoothed;
                 }
             }
         }
@@ -31,11 +36,13 @@ namespace DvMod.AirBrake
         {
             public static void Postfix(JObject carData, TrainCar __result)
             {
-                if (carData.TryGetValue(SaveKey, out var token))
+                if (carData.TryGetValue(SaveKey, out JToken token) && token is JObject obj)
                 {
                     var serializer = new JsonSerializer();
                     var state = ExtraBrakeState.Instance(__result.brakeSystem);
                     serializer.Populate(new JTokenReader(token), state);
+                    if (CarTypes.IsLocomotive(__result.carType) && obj.TryGetValue(mainReservoirKey, out var mainResPressure))
+                        __result.brakeSystem.mainReservoirPressure = __result.brakeSystem.mainReservoirPressureUnsmoothed = mainResPressure.Value<float>();
                     Main.DebugLog($"Loaded state for {carData["id"]}: {state}");
                 }
             }
